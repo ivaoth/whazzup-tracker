6a2c01499911c1cb6c1db811fc7b4d053532de15
Sarun Intaralawan <sarunint@sarunint.com>
2019-07-07 00:31:45 +0700

    fix: make scroll position restoration works

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
+import { ViewportScroller } from '@angular/common';
 import { ChangeDetectorRef, Component } from '@angular/core';
+import { Router, Scroll } from '@angular/router';
 import { Observable } from 'rxjs';
+import { delay, filter, first, switchMap, tap } from 'rxjs/operators';
 import { DataService } from '../data.service';
-import { first } from '../../../node_modules/rxjs/operators';
 import { DownloadService } from '../download.service';
 
 @Component({
@@ -12,8 +14,28 @@ import { DownloadService } from '../download.service';
 export class HomeComponent {
   data: Observable<any[]>;
   loadedData: any[];
-  constructor(private _data: DataService, private cd: ChangeDetectorRef, private download: DownloadService) {
+  constructor(
+    private _data: DataService,
+    private cd: ChangeDetectorRef,
+    private download: DownloadService,
+    private viewportScroller: ViewportScroller,
+    private router: Router
+  ) {
     this.data = _data.getData();
+    this.router.events
+      .pipe(
+        filter((e): e is Scroll => e instanceof Scroll),
+        filter(e => !!e.position),
+        switchMap(e => {
+          return this.data.pipe(
+            delay(0),
+            tap(_ => {
+              this.viewportScroller.scrollToPosition(e.position);
+            })
+          );
+        })
+      )
+      .subscribe();
   }
 
   onFileChanged(event) {
@@ -39,7 +61,9 @@ export class HomeComponent {
 
   downloadResult(valid: boolean) {
     this.data.pipe(first()).subscribe(d => {
-      const vids = this.uniq(d.filter(c => c.validated && c.valid === valid).map(s => s.vid));
+      const vids = this.uniq(
+        d.filter(c => c.validated && c.valid === valid).map(s => s.vid)
+      );
       this.download.download(vids.join('\n'), `output-${valid}.txt`);
     });
   }
